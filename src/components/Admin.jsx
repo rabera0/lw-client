99ecf42e16d3a4138604d32feec4e8b1b8efd406
Glitch (pinnate-uttermost-fiber) <none>
2024-08-08 01:39:35 +0000

    🎽🌜 Checkpoint ./client.js:69615091/1040 ./src/components/Admin.jsx:69615091/271

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from '../att-logo.png';
+import useWebSocket from '../useWebSocket';
 //import { Link } from 'react-router-dom';
 // import '../App.css';
 
 function Admin() {
+  const { message } = useWebSocket('wss://pinnate-uttermost-fiber.glitch.me/');
     return (
       <div className="Admin">
         <h2>Control Panel</h2>
         <br></br>
         <br></br>
+         {message && <p>Received from server: {message}</p>}
         <br></br>
         <br></br>    
       </div>
---
+++ src/useWebSocket.js
@@ -0,0 +1,35 @@
+// src/hooks/useWebSocket.js
+import { useState, useEffect } from 'react';
+
+const useWebSocket = (url) => {
+  const [ws, setWs] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const socket = new WebSocket(url);
+
+    socket.onopen = () => {
+      console.log('WebSocket connection established');
+      socket.send('Hello server!');
+    };
+
+    socket.onmessage = (event) => {
+      console.log('Received message from server:', event.data);
+      setMessage(event.data);
+    };
+
+    socket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    setWs(socket);
+
+    return () => {
+      socket.close();
+    };
+  }, [url]);
+
+  return { ws, message };
+};
+
+export default useWebSocket;
