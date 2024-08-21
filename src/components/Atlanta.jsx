d8b33326bdc1cb4480d0c471163e8912b50740a6
Glitch (pinnate-uttermost-fiber) <none>
2024-08-21 19:38:18 +0000

    🍚🎬 Checkpoint ./src/components/Atlanta.jsx:69615091/4838

--- src/components/Atlanta.jsx
+++ src/components/Atlanta.jsx
@@ -4,6 +4,7 @@ import uszips from '../data/USCities.json';
 import logo from '../att-logo.png';
 //import { Link } from 'react-router-dom';
 // import '../App.css';
+import { useState } from 'react';
 
 function findCityByZip(zipCode) {
   const numericZipCode = Number(zipCode);
@@ -11,14 +12,28 @@ function findCityByZip(zipCode) {
   return result ? result.city : 'ZIP code not found';
 }
 
-function Atlanta({ zip }) { 
+function Atlanta() { 
   const location = useLocation();
   const zipcode = location.state?.zipcode;
   
   const city = zipcode ? findCityByZip(zipcode) : 'ZIP code not provided';
 
+  // State to track iframe loading
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // Handle iframe load event
+  const handleLoad = () => {
+    setIsLoaded(true);
+  };
+
     return (
-      <div className="Atlanta">
+      <div className="Atlanta" style={{ position: 'relative', width: '100%', height: '100%' }}>
+      {!isLoaded && (
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          Loading...
+          {/* You can replace "Loading..." with a spinner or any other loading indicator */}
+        </div>
+      )}
 {/*        <h1>THE AT&T PERCH LIVING MURAL</h1>
         <h2> Connecting Neighborhoods... </h2>
        <p>Your zipcode is: {zipcode}</p> 
@@ -33,7 +48,16 @@ function Atlanta({ zip }) {
         <img src={logo} className="logo" alt="Logo" />
         <br></br>
         <br></br>  --> */}
-       <iframe class="iframe" style="visibility:hidden;" onload="this.style.visibility = 'visible';" src="https://readymag.website/u170488020/4927140/?link_target=parent" allowtransparency="true" frameborder="0" scrolling="no"></iframe>
+        <iframe
+        src="https://readymag.website/u170488020/4927140/?link_target=parent"
+        style={{ visibility: isLoaded ? 'visible' : 'hidden', transition: 'visibility 0s linear 0.5s' }}
+        onLoad={handleLoad}
+        allowTransparency="true"
+        frameBorder="0"
+        scrolling="no"
+        width="100%"
+        height="100%"
+      />
       </div>
       
     );
