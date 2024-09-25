a24ccb211008b17924493736660112cc39e46f60
Glitch (pinnate-uttermost-fiber) <none>
2024-09-25 12:49:02 +0000

    🐵🎺 Checkpoint ./src/components/IntlMap.jsx:69615091/2391 ./src/components/International.jsx:69615091/2823

--- src/components/IntlMap.jsx
+++ src/components/IntlMap.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 //import { Link } from 'react-router-dom';
 // import '../App.css';
 import logo from '../att-logo.png';
 import Footer from './Footer';
 import Header from './Header';
+import Graph from './Graph'; 
 
 function IntlMap() {
+    const navigate = useNavigate();
+  const location = useLocation();
+  const zipcode = location.state?.zipcode; // Get the zipcode from the state
+  const [isFading, setIsFading] = useState(false);
+  const [opacity, setOpacity] = useState(0); // State for fade-in
+
+  
+  useEffect(() => {
+    const fadeInTimer = setTimeout(() => {
+      setOpacity(1); // Set opacity to 1 after a delay to trigger fade-in
+    }, 100); // Delay before starting fade in
+
+    const timer = setTimeout(() => {
+      setIsFading(true);
+      setTimeout(() => {
+        navigate('/International');
+      }, 1000); // Duration of fade-out effect
+    }, 3500); // Wait for 3.5 seconds before starting the fade
+
+    return () => {
+      clearTimeout(fadeInTimer);
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+  
     return (
-      <div className="International">
+      <div className="International" style={{ opacity, transition: 'opacity 1s ease-in-out' }}>
         <h1>THE AT&T PERCH LIVING MURAL</h1>
         <h2> Welcome to America! </h2>
         <br></br>
