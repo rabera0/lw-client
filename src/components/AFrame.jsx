47bb5ed6383e6d1076383ea657828e59298868ba
Glitch (pinnate-uttermost-fiber) <none>
2024-09-30 17:38:27 +0000

    🖼🚖 Checkpoint ./src/components/AFrame.jsx:69615091/1198

--- src/components/AFrame.jsx
+++ src/components/AFrame.jsx
@@ -3,13 +3,13 @@ import React, { useEffect } from 'react';
 const AFrameScene = () => {
 
   return (
-    <a-scene>
-      <a-sky color="#2195CB"></a-sky>
+    <a-scene style={{ width: '80vw', height: '60vh', margin: '0 auto' }}>
+      <a-sky color="transparent"></a-sky>
       <a-camera position="0 0 3" user-height="0"></a-camera>
       <a-sphere
         src="https://raw.githubusercontent.com/aframevr/sample-assets/master/assets/images/space/earth_atmos_4096.jpg"
         radius="1"
-        segments-height="33">
+        segments-height="53">
         <a-animation
           attribute="rotation"
           dur="5000"
