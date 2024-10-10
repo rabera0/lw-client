c99a1a1ffff4b3b2e3a23b2a2f04d247405e26f2
Glitch (pinnate-uttermost-fiber) <none>
2024-10-10 13:47:08 -0400

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,6 @@ export default defineConfig({
   server: {
     host:"0.0.0.0",
     port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
+    strictPort: true
   }
 });
