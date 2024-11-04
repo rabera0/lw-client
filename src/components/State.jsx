ad7d6be75dffe6460f3cac308bf648deb6541939
rabera0 <rehoboth.abera@vanderbilt.edu>
2024-11-04 11:04:32 -0500

    test push

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -102,7 +102,7 @@ function State() {
       <div className="State" style={{ opacity, transition: 'opacity 1s ease-in-out' }}>
         <p>From     To</p>
          <br />
-        <h5>ATL {">>"} {state}</h5>
+        <h5>ATL → {state}</h5>
         <br />
         {/* Display the state message here */}
         <p>{message}</p>
